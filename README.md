2edbba099631da0943d012bc7616c0ff65615825
Dilawar Baloch <dilawarbalochdk107@gmail.com>
2024-10-07 14:51:26 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Simple_calcuter_dk" 
---
+++ main.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number:", type: "number", name: 'firstnumber' },
+    { message: 'enter second number', type: 'number', name: 'secondnumber' },
+    {
+        message: 'select one option for perform your work',
+        type: 'list',
+        name: "operator",
+        choices: ['addition', 'substraction', 'multiplication', "division"]
+    }
+]);
+if (answer.operator == 'addition') {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator == 'substraction') {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator == 'multiplication') {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log('please select this valid');
+}
